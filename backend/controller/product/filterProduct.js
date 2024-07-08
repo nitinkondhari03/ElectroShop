bb3070c6a8c6dc7d30a3e29d5b1dbfe231f00738
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-07-08 09:01:20 +0530

    add this files

--- backend/controller/product/filterProduct.js
+++ backend/controller/product/filterProduct.js
@@ -1,29 +1,28 @@
-const productModel = require("../../models/productModel")
+const productModel = require("../../models/productModel");
 
-const filterProductController = async(req,res)=>{
- try{
-        const categoryList = req?.body?.category || []
+const filterProductController = async (req, res) => {
+  try {
+    const categoryList = req?.body?.category || [];
 
-        const product = await productModel.find({
-            category :  {
-                "$in" : categoryList
-            }
-        })
+    const product = await productModel.find({
+      category: {
+        $in: categoryList,
+      },
+    });
 
-        res.json({
-            data : product,
-            message : "product",
-            error : false,
-            success : true
-        })
- }catch(err){
     res.json({
-        message : err.message || err,
-        error : true,
-        success : false
-    })
- }
-}
+      data: product,
+      message: "product",
+      error: false,
+      success: true,
+    });
+  } catch (err) {
+    res.json({
+      message: err.message || err,
+      error: true,
+      success: false,
+    });
+  }
+};
 
-
-module.exports = filterProductController
+module.exports = filterProductController;
