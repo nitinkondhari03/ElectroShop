d262c8cc7ea8af6c1637f4f63509bf5243309edc
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-07-10 10:22:04 +0530

    add frontend page

--- frontend/src/common/index.js
+++ frontend/src/common/index.js
@@ -37,6 +37,46 @@ const SummaryApi = {
     url: `${backendDomin}/api/update-product`,
     method: "post",
   },
+  categoryProduct: {
+    url: `${backendDomin}/api/get-categoryProduct`,
+    method: "get",
+  },
+  categoryWiseProduct: {
+    url: `${backendDomin}/api/category-product`,
+    method: "post",
+  },
+  productDetails: {
+    url: `${backendDomin}/api/product-details`,
+    method: "post",
+  },
+  addToCartProduct: {
+    url: `${backendDomin}/api/addtocart`,
+    method: "post",
+  },
+  addToCartProductCount: {
+    url: `${backendDomin}/api/countAddToCartProduct`,
+    method: "get",
+  },
+  addToCartProductView: {
+    url: `${backendDomin}/api/view-card-product`,
+    method: "get",
+  },
+  updateCartProduct: {
+    url: `${backendDomin}/api/update-cart-product`,
+    method: "post",
+  },
+  deleteCartProduct: {
+    url: `${backendDomin}/api/delete-cart-product`,
+    method: "post",
+  },
+  searchProduct: {
+    url: `${backendDomin}/api/search`,
+    method: "get",
+  },
+  filterProduct: {
+    url: `${backendDomin}/api/filter-product`,
+    method: "post",
+  },
 };
 
 export default SummaryApi;
