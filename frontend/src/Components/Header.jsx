3a632a418f1101f4f7a0bcba9fb315a5d75d0f51
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-07-23 11:06:30 +0530

    add this files

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -65,12 +65,12 @@ const Header = () => {
         <div className="hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2">
           <input
             type="text"
-            placeholder="search product here..."
+            placeholder="Search Product Here..."
             className="w-full outline-none"
             onChange={handleSearch}
             value={search}
           />
-          <div className="text-lg min-w-[50px] h-8 bg-red-600 flex items-center justify-center rounded-r-full text-white">
+          <div className="text-lg min-w-[50px] h-8 bg-blue-600 flex items-center justify-center rounded-r-full text-white">
             <GrSearch />
           </div>
         </div>
