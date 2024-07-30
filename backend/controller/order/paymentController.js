2e08a0feb961e62ce2224ec3560fbaaae0ce0aad
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-07-30 09:40:05 +0530

    add this file

--- backend/controller/order/paymentController.js
+++ backend/controller/order/paymentController.js
@@ -14,7 +14,7 @@ const paymentController = async(request,response)=>{
             billing_address_collection : 'auto',
             shipping_options : [
                 {
-                    shipping_rate : 'shr_1Phob4SJODKFPUo9ElyROEDk'
+                    shipping_rate : 'shr_1PhmIDSJODKFPUo9a2c2AlXQ'
                 }
             ],
             customer_email : user.email,
@@ -24,7 +24,7 @@ const paymentController = async(request,response)=>{
             line_items : cartItems.map((item,index)=>{
                 return{
                     price_data : {
-                      currency : 'usd',
+                      currency : 'inr',
                       product_data : {
                         name : item.productId.productName,
                         images : item.productId.productImage,
