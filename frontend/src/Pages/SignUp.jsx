1ed93b2b781d925bff6c7a22edb9f2587ecd4bd8
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-08-01 11:02:11 +0530

    Login with Google

--- frontend/src/Pages/SignUp.jsx
+++ frontend/src/Pages/SignUp.jsx
@@ -6,6 +6,7 @@ import { Link, useNavigate } from "react-router-dom";
 import imageTobase64 from "../helpers/imageTobase64";
 import SummaryApi from "../common";
 import { toast } from "react-toastify";
+import GoogleAuth from "../Components/GoogleAuth";
 
 const SignUp = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -154,9 +155,7 @@ const SignUp = () => {
               Sign Up
             </button>
             <h1 className="text-center">OR</h1>
-            <button className="bg-red-700 text-white rounded-lg p-3 uppercase hover:opacity-95">
-              Continue with google
-            </button>
+            <GoogleAuth/>
           </form>
 
           <p className="my-5">
