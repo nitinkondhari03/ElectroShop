9ab766a53249471d394288193c457678030bf7e0
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-09-02 15:01:34 +0530

    add this files

--- frontend/src/Components/OrderAddress.jsx
+++ frontend/src/Components/OrderAddress.jsx
@@ -8,7 +8,6 @@ import { MdDelete } from "react-icons/md";
 import SummaryApi from "../common";
 import { toast } from "react-toastify";
 import { Country, State, City } from "country-state-city";
-
 const OrderAddress = ({ onClose }) => {
   const [data, setData] = useState({
     productName: "",
@@ -27,7 +26,7 @@ const OrderAddress = ({ onClose }) => {
   const [country, setCountry] = useState("");
   const [pinCode, setPinCode] = useState("");
   const [phoneNo, setPhoneNo] = useState("");
-
+  
   const handleOnChange = (e) => {
     const { name, value } = e.target;
 
@@ -110,147 +109,8 @@ const OrderAddress = ({ onClose }) => {
           className="grid p-4 gap-2 overflow-y-scroll h-full pb-5"
           onSubmit={handleSubmit}
         >
-          <label htmlFor="productName">Product Name :</label>
-          <input
-            type="text"
-            id="productName"
-            placeholder="enter product name"
-            name="productName"
-            value={data.productName}
-            onChange={handleOnChange}
-            className="p-2 bg-slate-100 border rounded"
-            required
-          />
-
-          <label htmlFor="brandName" className="mt-3">
-            Brand Name :
-          </label>
-          <input
-            type="text"
-            id="brandName"
-            placeholder="enter brand name"
-            value={data.brandName}
-            name="brandName"
-            onChange={handleOnChange}
-            className="p-2 bg-slate-100 border rounded"
-            required
-          />
-
-          <label htmlFor="category" className="mt-3">
-            Category :
-          </label>
-          <select
-            required
-            value={data.category}
-            name="category"
-            onChange={handleOnChange}
-            className="p-2 bg-slate-100 border rounded"
-          >
-            <option value={""}>Select Category</option>
-            {productCategory.map((el, index) => {
-              return (
-                <option value={el.value} key={el.value + index}>
-                  {el.label}
-                </option>
-              );
-            })}
-          </select>
-
-          <label htmlFor="productImage" className="mt-3">
-            Product Image :
-          </label>
-          <label htmlFor="uploadImageInput">
-            <div className="p-2 bg-slate-100 border rounded h-32 w-full flex justify-center items-center cursor-pointer">
-              <div className="text-slate-500 flex justify-center items-center flex-col gap-2">
-                <span className="text-4xl">
-                  <FaCloudUploadAlt />
-                </span>
-                <p className="text-sm">Upload Product Image</p>
-                <input
-                  type="file"
-                  id="uploadImageInput"
-                  className="hidden"
-                  onChange={handleUploadProduct}
-                />
-              </div>
-            </div>
-          </label>
-          <div>
-            {data?.productImage[0] ? (
-              <div className="flex items-center gap-2">
-                {data.productImage.map((el, index) => {
-                  return (
-                    <div className="relative group">
-                      <img
-                        src={el}
-                        alt={el}
-                        width={80}
-                        height={80}
-                        className="bg-slate-100 border cursor-pointer"
-                        onClick={() => {
-                          setOpenFullScreenImage(true);
-                          setFullScreenImage(el);
-                        }}
-                      />
-
-                      <div
-                        className="absolute bottom-0 right-0 p-1 text-white bg-red-600 rounded-full hidden group-hover:block cursor-pointer"
-                        onClick={() => handleDeleteProductImage(index)}
-                      >
-                        <MdDelete />
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            ) : (
-              <p className="text-red-600 text-xs">
-                *Please upload product image
-              </p>
-            )}
-          </div>
-
-          <label htmlFor="price" className="mt-3">
-            Price :
-          </label>
-          <input
-            type="number"
-            id="price"
-            placeholder="enter price"
-            value={data.price}
-            name="price"
-            onChange={handleOnChange}
-            className="p-2 bg-slate-100 border rounded"
-            required
-          />
-
           <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
-          </label>
-          <input
-            type="number"
-            id="sellingPrice"
-            placeholder="enter selling price"
-            value={data.sellingPrice}
-            name="sellingPrice"
-            onChange={handleOnChange}
-            className="p-2 bg-slate-100 border rounded"
-            required
-          />
-
-          <label htmlFor="description" className="mt-3">
-            Description :
-          </label>
-          <textarea
-            className="h-28 bg-slate-100 border resize-none p-1"
-            placeholder="enter product description"
-            rows={3}
-            onChange={handleOnChange}
-            name="description"
-            value={data.description}
-          ></textarea>
-          <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
+            Address
           </label>
           <input
             type="text"
@@ -262,7 +122,7 @@ const OrderAddress = ({ onClose }) => {
           />
 
           <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
+            Pin Code
           </label>
           <input
             type="number"
@@ -274,11 +134,11 @@ const OrderAddress = ({ onClose }) => {
           />
 
           <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
+            Mobile Number
           </label>
           <input
             type="number"
-            placeholder="Phone Number"
+            placeholder="Mobile Number"
             required
             value={phoneNo}
             onChange={(e) => setPhoneNo(e.target.value)}
@@ -287,7 +147,7 @@ const OrderAddress = ({ onClose }) => {
           />
 
           <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
+            Country
           </label>
           <select
             required
@@ -304,7 +164,7 @@ const OrderAddress = ({ onClose }) => {
               ))}
           </select>
           <label htmlFor="sellingPrice" className="mt-3">
-            Selling Price :
+          State
           </label>
           {country && (
             <select
@@ -332,7 +192,7 @@ const OrderAddress = ({ onClose }) => {
               onChange={(e) => setCity(e.target.value)}
               className="p-2 bg-slate-100 border rounded"
             >
-              <option value="">City</option>
+              <option value="">District</option>
               {City &&
                 City.getCitiesOfState(country, state).map((item) => (
                   <option key={item.isoCode} value={item.isoCode}>
