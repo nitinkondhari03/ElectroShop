11d5c283813262185998e0f7ee61c64f685ffac2
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-10-01 12:42:51 +0530

    add this file

--- frontend/src/Pages/CategoryProduct.jsx
+++ frontend/src/Pages/CategoryProduct.jsx
@@ -9,7 +9,7 @@ const CategoryProduct = () => {
   const loadingList = new Array(13).fill(null);
   const [data, setData] = useState([]);
   const navigate = useNavigate();
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
   const [filters, setfilters] = useState(false);
   const location = useLocation();
   const urlSearch = new URLSearchParams(location.search);
@@ -24,7 +24,7 @@ const CategoryProduct = () => {
   const [sortBy, setSortBy] = useState("");
 
   const fetchData = async () => {
-    console.log(SummaryApi.filterProduct.url)
+    setLoading(true);
     const response = await fetch(SummaryApi.filterProduct.url, {
       method: SummaryApi.filterProduct.method,
       headers: {
@@ -37,8 +37,9 @@ const CategoryProduct = () => {
 
     const dataResponse = await response.json();
     let x = dataResponse?.data || [];
-    console.log(x)
+    console.log(x);
     setData(x);
+    setLoading(false);
   };
 
   const handleSelectCategory = (e) => {
@@ -52,10 +53,7 @@ const CategoryProduct = () => {
     });
   };
 
-
-
   useEffect(() => {
-    setLoading(true);
     const arrayOfCategory = Object.keys(selectCategory)
       ?.map((categoryKeyName) => {
         if (selectCategory[categoryKeyName]) {
@@ -74,19 +72,15 @@ const CategoryProduct = () => {
       }
       return `category=${el}&&`;
     });
-    setLoading(false);
-    console.log(urlFormat)
+
     navigate("/product-category?" + urlFormat.join(""));
   }, [selectCategory]);
 
   useEffect(() => {
-    setLoading(true);
     fetchData();
-    setLoading(false);
   }, [filterCategoryList]);
 
   const handleOnChangeSortBy = (e) => {
-    setLoading(true);
     const { value } = e.target;
 
     setSortBy(value);
@@ -98,15 +92,12 @@ const CategoryProduct = () => {
     if (value === "dsc") {
       setData((preve) => preve.sort((a, b) => b.sellingPrice - a.sellingPrice));
     }
-    setLoading(false);
   };
 
   useEffect(() => {}, [sortBy]);
 
   const handleFilter = () => {
-    setLoading(true);
     setfilters(!filters);
-    setLoading(false);
   };
 
   return (
@@ -262,9 +253,34 @@ const CategoryProduct = () => {
               </div>
             </div>
           )}
-          <div className="min-h-[calc(100vh-120px)] overflow-y-scroll max-h-[calc(100vh-120px)]">
-            <VerticalCard data={data} loading={loading} />
-          </div>
+          {loading && (
+            <div className="min-h-[calc(100vh-120px)] overflow-y-scroll max-h-[calc(100vh-120px)]">
+              <div className="grid lg:grid-cols-3 gap-10 sm:grid-cols-2">
+                {loading &&
+                  loadingList?.map((product, index) => {
+                    return (
+                      <div key={index} className="shadow">
+                        <div className="bg-slate-200 h-48 p-4 flex justify-center items-center animate-pulse"></div>
+                        <div className="p-4 grid gap-3">
+                          <h2 className="font-medium text-base md:text-lg text-ellipsis line-clamp-1 text-black p-1 py-2 animate-pulse rounded-full bg-slate-200"></h2>
+                          <p className="capitalize text-slate-500 p-1 animate-pulse rounded-full bg-slate-200  py-2"></p>
+                          <div className="flex gap-3">
+                            <p className="text-red-600 font-medium p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
+                            <p className="text-slate-500 line-through p-1 animate-pulse rounded-full bg-slate-200 w-full  py-2"></p>
+                          </div>
+                          <button className="text-sm  text-white px-3  rounded-full bg-slate-200  py-2 animate-pulse"></button>
+                        </div>
+                      </div>
+                    );
+                  })}
+              </div>
+            </div>
+          )}
+          {!loading && (
+            <div className="min-h-[calc(100vh-120px)] overflow-y-scroll max-h-[calc(100vh-120px)]">
+              <VerticalCard data={data} />
+            </div>
+          )}
         </div>
       </div>
     </div>
