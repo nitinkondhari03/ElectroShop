c83da8b9362f1bbf11f6725e6b119547b7e87fc1
Nitin Kondhari <nitinkondhari85@gmail.com>
2024-10-28 10:22:09 +0530

    add this file

--- backend/controller/user/forgotPassword.js
+++ backend/controller/user/forgotPassword.js
@@ -18,12 +18,53 @@ async function forgotPassword(req, res) {
   });
 
   const resetPasswordUrl = `${process.env.FRONTEND_URL}/resetPassword/${token}`;
-
-  const message = `Your password reset token is :- \n\n ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`;
+  const username = user.name;
+  // const message = `Your password reset token is :- \n\n  ${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.'<button>Reset Password button>'`;
+  const message = `<div>
+      <div>
+      <img
+        src="https://mern-liard-kappa.vercel.app/static/media/Online%20Shop%20Logo.8a37346d4b136f5f5dcd.png"
+        width="50px"
+        alt="img"
+        style="transition: all;mix-blend-mode: multiply;"
+      />
+      <p style="margin-top: -15px;">Electro Shop</p>
+      
+     </div>
+      <div style="box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;width: auto;">
+      <p style="font-size:xx-large;color: rgb(82, 80, 80);">Hello ${username}</p>
+      <p style="font-weight: bolder; font-size: large; font-family: 'Courier New', Courier, monospace;">
+        A request has been received to change the Password for your Electro shop
+        account.
+      </p>
+      <a href="${resetPasswordUrl}"
+        ><button
+          style="
+            background-color: rgb(34, 87, 107);
+            color: white;
+            font-size: 20px;
+            padding: 1%;
+            border-radius: 12px;font-family: 'Courier New', Courier
+          "
+        >
+          Reset Password
+        </button></a
+      >
+      <p style="font-size: large; font-family: 'Courier New', Courier, monospace; font-size: medium;">
+        if you did not initiate this request , please contact us immediately at
+        electroshop@gmail.com
+      </p>
+      <p style="font-size: large; font-family: 'Courier New', Courier, monospace;font-size: medium;">
+        Thank you
+        <br />
+        The Electro Shop Team
+      </p>
+</div>
+    </div>`;
   try {
     await sendEmail({
       email: user.email,
-      subject: `Ecommerce Password Recovery`,
+      subject: `Electro Shop Password Recovery`,
       message,
     });
 
--- backend/helpers/sendEmail.js
+++ backend/helpers/sendEmail.js
@@ -16,6 +16,7 @@ const sendEmail = async (options) => {
     to: options.email,
     subject: options.subject,
     text: options.message,
+    html:options.message,
   };
 
   await transporter.sendMail(mailOptions);
